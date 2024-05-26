083a105c44ec9a8da9cf639b933241f45751de3d
kimagin <info@kimagination.net>
2024-05-26 15:54:12 +0200

    basic log function

---
+++ src/functions/log.ts
@@ -0,0 +1,50 @@
+type LogLevel = 'log' | 'info' | 'warn' | 'error'
+
+interface LogOptions {
+  label?: string
+  level?: LogLevel
+  time?: boolean
+}
+
+/**
+ *
+ * ---
+ * **Description:**
+ * Logs a message with optional label, level, and timestamp.
+ * ---
+ * **Example usage:**
+ * ```js
+ * const originalText = "Hello World";
+ * const logOptions = { level: 'info', label: '', time: false };
+ * log(originalText, logOptions);
+ * console.log("hello-world");
+ * ```
+ * ---
+ *
+ * @param {string} content - The content of the log message.
+ * @param {LogOptions} [options] - Options for logging.
+ * @param {string} [options.label] - A string label for the log message.
+ * @param {LogLevel} [options.level] - The level of the log message (log, info, warn, or error).
+ * @param {boolean} [options.time=false] - Whether to include a timestamp in the log message.
+ * @returns {void}
+ */
+export function log(content: string, options: LogOptions = {}): void {
+  const { level = 'log', label = '', time = false } = options
+  const timestamp = time ? new Date().toLocaleTimeString() : ''
+  const logMessage = `${timestamp} ${label && `[${label}]`} ${content}`.trim()
+
+  const levels: Record<
+    LogLevel,
+    { method: (...args: any[]) => void; color: string }
+  > = {
+    log: { method: console.log, color: '#A9A9A9' },
+    info: { method: console.info, color: '#12b85b' },
+    warn: { method: console.warn, color: '#FFD700' },
+    error: { method: console.error, color: '#FFA07A' },
+  }
+
+  const { method, color } = levels[level]
+  const style = `color: ${color}; font-weight: bold; font-style: italic;`
+
+  method(`%c${logMessage}`, style)
+}
