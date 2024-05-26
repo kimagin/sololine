132472bd7f0ba040a6db2392344575d7b8596c83
kimagin <info@kimagination.net>
2024-05-26 15:54:46 +0200

    basic email-validation function

--- src/functions/email-validation.ts
+++ src/functions/email-validation.ts
@@ -1,12 +1,12 @@
-// create a type script email validation
 /**
- * Validates whether a given string is a valid email address.
+ * **Description:**
+ * ---
  *
- * @param {string} email - The email address to validate.
- * @returns {boolean} True if the email is valid, false otherwise.
+ * Validates whether a given string is a valid email address.
+ * This function uses a regular expression to validate whether the given string is in the format of an email address. It checks for the presence of '@', '.', and alphanumeric characters.
  *
  * **Example Usage:**
- *
+ * ---
  * ```javascript
  * import { _isEmail } from './validateEmail';
  *
@@ -17,8 +17,9 @@
  *   console.log('The email address is invalid');
  * }
  * ```
+ * @param {string} email **The email address to validate.**
  *
- * @description This function uses a regular expression to validate whether the given string is in the format of an email address. It checks for the presence of '@', '.', and alphanumeric characters.
+ * @returns {boolean} **True if the email is valid, false otherwise.**
  */
 export function isEmail(email: string): boolean {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
